fa3d51ded708e265da96537443ca0e1b4f0c68e7
AKASH-code-stack <jeyakash71012@gmail.com>
2025-09-19 09:59:59 +0530

    Add files via upload

---
+++ ques 6.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    // Input three numbers
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Compare and find the greatest
+    if (a >= b && a >= c) {
+        printf("The greatest number is: %d\n", a);
+    }
+    else if (b >= a && b >= c) {
+        printf("The greatest number is: %d\n", b);
+    }
+    else {
+        printf("The greatest number is: %d\n", c);
+    }
+
+    return 0;
+}
